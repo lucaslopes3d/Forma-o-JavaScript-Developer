11c2bdec22e0e505c1f4ba63619ae5d1bcd1d2dc
Lucas Lopes <3d.lucaslopes@gmail.com>
2023-12-12 12:40:53 -0300

    Atualizando e atualizando arquivos

---
+++ Arrays/arrays_geral.js
@@ -0,0 +1,30 @@
+/*const alunos = ["Lucas Lopes", "tarde demais", "Ahhhhh"]
+const lucas = 'Lucas Lopes Lima'*/
+
+/*for(let i = 0; i < alunos.length; i++){
+    console.log (alunos[i])
+}
+
+for(let a = 0; a < lucas.length; a++){
+    console.log(lucas[a])
+}*/
+
+const notas = []
+
+notas.push(8)
+notas.push(8)
+notas.push(5)
+notas.push(3)
+notas.push(9)
+notas.push(10)
+
+let soma = 0
+
+for(let i = 0; i < notas.length; i++){
+    const nota = notas[i]
+    soma += nota
+}
+
+const media = (soma / notas.length).toFixed(2)
+
+console.log(media)
